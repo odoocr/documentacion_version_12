0f7ff3b380cfb00d6cbeff21a6a4d39ea8b01af5
Elías Torres <eliastorres@gmail.com>
2021-07-12 11:52:10 -0600

    Ajustes a odoo.sh para compatibilidad con Debian 10 y mejoras
    Compatibilidad con Debian 10
    Mejor instalación de dependencias de wkhtmltopdf
    Sufijo opcional para instalación de múltiples instancias
    Versión de Odoo pasada como parámetro opcional para cuando las nuevas versiones de l10n_cr estén disponibles

--- odoo12.sh
+++ odoo12.sh
@@ -3,54 +3,41 @@
 # AVISO IMPORTANTE!!! (WARNING!!!)
 # ASEGURESE DE TENER UN SERVIDOR / VPS CON AL MENOS > 2GB DE RAM
 # You must to have at least > 2GB of RAM
-# Ubuntu 18.04, 19, 20.04 LTS tested
+# Ubuntu 18.04, 20.04 LTS tested
+# Debian 10
 # v2.2
-# Last updated: 17-10-2020
+# Last updated: 2021-07-12
+# Usage: ./odoo12.sh 12 name port
+# To create a service called odoo12name running on port
 
 OS_NAME=$(lsb_release -cs)
-usuario=$USER
 DIR_PATH=$(pwd)
-VCODE=12
-VERSION=12.0
-PORT=8069
+VCODE=${1:-12}
+VERSION=${VCODE}.0
+PORT=${3:-8069}
+PORT_LONG=$((PORT+100))
 DEPTH=1
-PROJECT_NAME=odoo$VCODE
+PROJECT_NAME=odoo$VCODE${2:-}
 PATHBASE=/opt/$PROJECT_NAME
+ODOO_CONF=$PATHBASE/config/odoo.conf
 PATH_LOG=$PATHBASE/log
 PATHREPOS=$PATHBASE/extra-addons
 PATHREPOS_OCA=$PATHREPOS/oca
+ADMIN_PASS=$( cat /dev/urandom | tr -cd "[:alnum:]" | head -c 22 )
 
-if [[ $OS_NAME == "disco" ]];
+sudo adduser --system --quiet --shell=/bin/sh --home=$PATHBASE --gecos "$PROJECT_NAME" --group $PROJECT_NAME
 
-then
-	echo $OS_NAME
-	OS_NAME="bionic"
+#sudo adduser $usuario sudo
 
-fi
-
-if [[ $OS_NAME == "focal" ]];
-
-then
-	echo $OS_NAME
-	OS_NAME="bionic"
-
-fi
-
-wk64="https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1."$OS_NAME"_amd64.deb"
-wk32="https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1."$OS_NAME"_i386.deb"
-
-sudo adduser --system --quiet --shell=/bin/bash --home=$PATHBASE --gecos 'ODOO' --group $usuario
-sudo adduser $usuario sudo
-
-# add universe repository & update (Fix error download libraries)
-sudo add-apt-repository universe
+# add universe repository & update (Fix error download libraries), only for Ubuntu
+test Ubuntu = `lsb_release -is` && sudo add-apt-repository universe
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y git
 
 # Update and install Postgresql
 sudo apt-get install postgresql -y
-sudo su - postgres -c "createuser -s $usuario"
+sudo su - postgres -c "createuser -s $PROJECT_NAME"
 
 sudo mkdir $PATHBASE
 sudo mkdir $PATHREPOS
@@ -71,33 +58,29 @@ sudo apt-get -y install gcc python3-dev libxml2-dev libxslt1-dev \
 
 sudo apt-get -y install python3 python3-pip python3-setuptools htop unzip
 
-# FIX wkhtml* dependencie Ubuntu Server 18.04
-sudo apt-get -y install libxrender1
-
 # Install nodejs and less
 sudo apt-get install -y npm node-less
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install -g less
 
 # Download & install WKHTMLTOPDF
-sudo wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb $PATHBASE
+WK_VER=0.12.5
+WK_REL=${WK_VER}-1
+WK_DEB=wkhtmltox_0.12.5-1.${OS_NAME}_$(dpkg --print-architecture).deb
 
-if [[ "`getconf LONG_BIT`" == "32" ]];
+sudo wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/$WK_VER/$WK_DEB
 
-then
-	sudo wget $wk32
-else
-	sudo wget $wk64
-fi
+# Install dependencies:
+apt-get install -y $(dpkg -I $WK_DEB | grep Depends | cut -d: -f2 | sed 's/,//g')
 
-sudo dpkg -i --force-depends $PATHBASE/wkhtmltox_0.12.5-1*.deb
+sudo dpkg -i $WK_DEB
 sudo apt-get -f -y install
+# ¿Necesario?
 sudo ln -s /usr/local/bin/wkhtml* /usr/bin
-sudo rm $PATHBASE/wkhtmltox*.deb
-sudo apt-get -f -y install
+sudo rm $WK_DEB
 
 # install python requirements file (Odoo)
-sudo chown -R $usuario: $PATHBASE
+sudo chown -R $PROJECT_NAME: $PATHBASE
 sudo sed -i "s/psycopg2==2.7.3.1; sys_platform != 'win32'/psycopg2==2.7.3.1; sys_platform != 'win32' and python_version < '3.8'/g" $PATHBASE/odoo/requirements.txt
 sudo sed -i "s/psycopg2==2.8.3; sys_platform == 'win32'/psycopg2==2.8.3; sys_platform == 'win32' or python_version >= '3.8'/g" $PATHBASE/odoo/requirements.txt
 sudo sed -i '/libsass/d' $PATHBASE/odoo/requirements.txt
@@ -107,18 +90,18 @@ sudo pip3 install -r $PATHBASE/odoo/requirements.txt
 cd $DIR_PATH
 
 sudo mkdir $PATHBASE/config
-sudo rm $PATHBASE/config/odoo$VCODE.conf
-sudo touch $PATHBASE/config/odoo$VCODE.conf
+sudo rm -f $ODOO_CONF
+sudo touch $ODOO_CONF
 echo "
 [options]
 ; This is the password that allows database operations:
-;admin_passwd =
+admin_passwd = $ADMIN_PASS
 db_host = False
 db_port = False
 ;db_user =
 ;db_password =
 data_dir = $PATHBASE/data
-logfile= $PATH_LOG/odoo$VCODE.log
+logfile= $PATH_LOG/odoo.log
 
 ############# addons path ######################################
 
@@ -131,7 +114,7 @@ addons_path =
 #################################################################
 
 xmlrpc_port = $PORT
-longpolloing_port = 8072
+longpolloing_port = $PORT_LONG
 ;dbfilter = ^%d$
 ;dbfilter = .*
 ;proxy_mode = True
@@ -141,30 +124,33 @@ limit_time_real = 6000
 limit_time_cpu = 6000
 limit_memory_soft = 3355443200
 limit_memory_hard = 4026531840
-" | sudo tee --append $PATHBASE/config/odoo$VCODE.conf
+;proxy_mode = True
+" | sudo tee --append $ODOO_CONF
 
-sudo rm /etc/systemd/system/odoo$VCODE.service
-sudo touch /etc/systemd/system/odoo$VCODE.service
-sudo chmod +x /etc/systemd/system/odoo$VCODE.service
+sudo chown -R $PROJECT_NAME: $PATHBASE
+
+sudo rm -f /etc/systemd/system/$PROJECT_NAME.service
+sudo touch /etc/systemd/system/$PROJECT_NAME.service
+sudo chmod +x /etc/systemd/system/$PROJECT_NAME.service
 echo "
 [Unit]
-Description=Odoo$VCODE
+Description=Odoo instance: $PROJECT_NAME
 After=postgresql.service
 
 [Service]
 Type=simple
-User=$usuario
-ExecStart=$PATHBASE/odoo/odoo-bin --config $PATHBASE/config/odoo$VCODE.conf
+User=$PROJECT_NAME
+ExecStart=$PATHBASE/odoo/odoo-bin --config $ODOO_CONF
 
 [Install]
 WantedBy=multi-user.target
-" | sudo tee --append /etc/systemd/system/odoo$VCODE.service
+" | sudo tee --append /etc/systemd/system/$PROJECT_NAME.service
 sudo systemctl daemon-reload
-sudo systemctl enable odoo$VCODE.service
-sudo systemctl start odoo$VCODE
+sudo systemctl enable $PROJECT_NAME.service
+sudo systemctl start $PROJECT_NAME
 
-sudo chown -R $usuario: $PATHBASE
 
-echo "Odoo $VERSION Installation has finished!! ;) by crfactura.com"
+echo "Odoo instance $PROJECT_NAME on port $PORT has finished! Special thanks to crfactura.com" >&2
 IP=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f7)
-echo "You can access from: http://$IP:$PORT  or http://localhost:$PORT"
+echo "You can access it at: http://$IP:$PORT  or http://localhost:$PORT"
+echo "Admin pass: $ADMIN_PASS ( stored in $ODOO_CONF )"
